8c77bd6bd7e8bfe1365a1723145b725ad77454a0
Simon Oakes <813389+soakes@users.noreply.github.com>
2026-06-23 12:03:59 +0100

    chore: bump version to 1.0.1

--- s3mirror.py
+++ s3mirror.py
@@ -31,7 +31,7 @@ import yaml
 from boto3.s3.transfer import TransferConfig
 from botocore.config import Config
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 # ==========================================
 # DEFAULT CONFIGURATION
